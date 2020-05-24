1bd08a5e2d4ac004b86bf8dfe2e02b30ade21a58
Roque <Roque.m.sanjuan@gmail.com>
2020-05-24 19:49:37 -0300

    Primer Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+    res.send('Hola mundo');
+});
+
+
+
+app.listen(8000, function(){
+    console.log("Escuchando por el puerto 8000");
+});
